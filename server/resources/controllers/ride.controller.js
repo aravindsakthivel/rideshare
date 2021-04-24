1dc4c984fa0783229f833bc5223029c3a5f24876
Arjun K Prasad <arjun1237.kp@gmail.com>
2021-04-25 02:03:18 +0530

    changes to ride controller setup

--- server/resources/controllers/ride.controller.js
+++ server/resources/controllers/ride.controller.js
@@ -1,10 +1,18 @@
 const Ride = require('../models/ride.model')
 
 const get_all_previous_rides_taken_by_user = async (req, res) => {
-
+    
 }
 
 const get_all_previous_rides_by_user = async (req, res) => {
+
+}
+
+const get_current_ride_by_user = async (req, res) => {
+
+}
+
+const get_current_ride_taken_by_user = async (rq, res) => {
 
 }
 
@@ -25,5 +33,7 @@ module.exports = {
     get_all_previous_rides_by_user,
     change_current_location,
     change_ride_status,
-    create_a_ride
+    create_a_ride,
+    get_current_ride_by_user,
+    get_current_ride_taken_by_user
 }
--- server/resources/routes/ride.route.js
+++ server/resources/routes/ride.route.js
@@ -1,11 +1,21 @@
-const { get_all_previous_rides_taken_by_user, get_all_previous_rides_by_user, create_a_ride, change_ride_status, change_current_location } = require('../controllers/ride.controller')
+const { 
+    get_all_previous_rides_taken_by_user, 
+    get_all_previous_rides_by_user, 
+    create_a_ride, 
+    change_ride_status, 
+    change_current_location, 
+    get_current_ride_by_user, 
+    get_current_ride_taken_by_user 
+} = require('../controllers/ride.controller')
 
 const ride_route = require('express').Router()
 
 ride_route.post('/', create_a_ride)
 ride_route.patch('/status/:id', change_ride_status)
 ride_route.patch('/location/:id', change_current_location)
-ride_route.get('/by/:user_id', get_all_previous_rides_by_user)
-ride_route.get('/taken/:user_id', get_all_previous_rides_taken_by_user)
+ride_route.get('/by/:user_id', get_current_ride_by_user)
+ride_route.get('/taken/:user_id', get_current_ride_taken_by_user)
+ride_route.get('/previous/by/:user_id', get_all_previous_rides_by_user)
+ride_route.get('/previous/taken/:user_id', get_all_previous_rides_taken_by_user)
 
 module.exports = ride_route
